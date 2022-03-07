72c8b63a37e59cc067ff4766f44cfe9158e95d6b
woodyamazing <92947585+woodyamazing@users.noreply.github.com>
2022-03-07 15:27:07 +0100

    Create tpwoodyll-macos.py

---
+++ tpwoodyll-macos.py
@@ -0,0 +1,25 @@
+keybind = "§" #set your keybind here
+
+
+import pyperclip as pc
+import keyboard as k
+import time
+
+while True:
+    k.wait(keybind)
+    coords = pc.paste()
+
+    k.send('cmd+tab')
+    time.sleep(0.5)
+    k.send("t")
+    time.sleep(0.05)
+    k.send("backspace")
+    k.write("/tpll "+ coords)
+    k.press("enter")
+    time.sleep(0.1)
+    k.press_and_release("t")
+    time.sleep(0.05)
+    k.send("backspace")
+    k.write("/up 0")
+    k.press("enter")
+    k.send('cmd+tab')
