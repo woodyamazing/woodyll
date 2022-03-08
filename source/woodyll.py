1d8df4e19d5d7a7f44d8b48660e417630f199781
woodyamazing <92947585+woodyamazing@users.noreply.github.com>
2022-03-08 15:40:06 +0100

    Create woodyll.py

---
+++ source/woodyll.py
@@ -0,0 +1,54 @@
+import subprocess
+import sys
+import importlib
+import time
+import os
+import json
+
+def install(package):
+	subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+install("pyperclip")
+install("keyboard")
+
+pc = importlib.import_module('pyperclip')
+k = importlib.import_module('keyboard')
+
+configFile = __file__[0:-17] + 'config.json'  # path to config file
+
+# load config file
+with open(configFile, 'r') as config:
+	configData = json.load(config)
+	delay = int(configData["delay"])
+	keybind = configData["keybind"]
+
+# detect OS
+if os.name == "posix":
+	appSwitch = "cmd+tab"
+
+if os.name == "nt":
+	appSwitch = "alt+tab"
+
+os.system('cls' if os.name == 'nt' else 'clear')
+print("tpwoodyll v1.1 \nReady")
+
+while True:
+	k.wait(keybind)
+
+	coords = pc.paste()
+
+	k.send(appSwitch)
+	time.sleep(delay)
+	k.send('esc')
+	k.send('t')
+	time.sleep(0.05)
+	k.send('backspace')
+	k.write('/tpll' + coords)
+	k.send('t')
+	time.sleep(0.05)
+	k.send('backspace')
+	k.write('/up 0')
+	k.send('enter')
+	k.send(appSwitch)
+
